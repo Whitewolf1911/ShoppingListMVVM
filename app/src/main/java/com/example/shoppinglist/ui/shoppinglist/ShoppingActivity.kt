0a5ce5d093fa1037a9585dce8907b0bd2a7a2d6f
Whitewolf1911 <alibasoglu1911@gmail.com>
2022-06-18 12:40:33 +0300

    Kodein Injection Added

--- app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -12,8 +12,16 @@ import com.example.shoppinglist.data.db.entities.ShoppingItem
 import com.example.shoppinglist.data.repositories.ShoppingRepository
 import com.example.shoppinglist.databinding.ActivityShoppingBinding
 import com.example.shoppinglist.other.ShoppingItemAdapter
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class ShoppingActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity(), KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: ShoppingViewModelFactory by instance()
+
     private lateinit var binding: ActivityShoppingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +33,10 @@ class ShoppingActivity : AppCompatActivity() {
         //we will solve this with Kodein Dependency Injection
         //Todo use kodein injection
 
-        val database = ShoppingDatabase(this)
-        val repository = ShoppingRepository(database)
-        val factory = ShoppingViewModelFactory(repository)
+//        val database = ShoppingDatabase(this)
+//        val repository = ShoppingRepository(database)
+//        val factory = ShoppingViewModelFactory(repository)
+
         val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
 
         val adapter = ShoppingItemAdapter(listOf(), viewModel)
